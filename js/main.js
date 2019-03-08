5352d06573c5531aa4ee367704fdff20c52697f1
Graphic <graphic@IMM-Instructor.local>
2019-03-08 13:27:54 -0500

    Turn array of strings into objects (table)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 const shopping = document.getElementById(`shopping`);
-const fruits = [`apples`, `bananas`, `strawberries`];
-fruits.push(`grapes`);
-fruits.push(`kiwi`);
 
-shopping.innerHTML = fruits.map(frt => `<li>I need ${frt}.</li>`).join('');
+const fruits = [
+    { name: `apples`, qty: 3, cat: `fruit` },
+    { name: `bananas`, qty: 1, cat: `fruit` },
+    { name: `strawberries`, qty: 10, cat: `fruit` },
+];
+
+
+shopping.innerHTML = fruits.map(frt => `<li>I need ${frt.qty} ${frt.name}.</li>`).join('');
 
